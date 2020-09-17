108ecf5e269cd2af8cf5db8726ff50bfd13dc53f
kongdd.sysu <kongdd.sysu@gmail.com>
2020-09-17 06:03:29 +0000

    Updated 00_visualization.js (added 6 lines, deleted 3 lines).

--- 00_visualization.js
+++ 00_visualization.js
@@ -4,8 +4,11 @@ var imgcol = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/PML_
 var pkg_vis = require('users/kongdd/public:pkg_vis.js');
 
 var vis_et = { min: 100, max: 1600, palette: pkg_vis.colors.RdYlBu[11] };
-var lg_et = pkg_vis.grad_legend(vis_et, 'ET', true); 
+var lg_et = pkg_vis.grad_legend(vis_et, 'ET', false); 
 
-// var vis_gpp     = {min: 100, max: 3500, palette:pkg_vis.colors.RdYlGn[11]};
-// var lg_gpp      = pkg_vis.grad_legend(vis_gpp, 'GPP', true); 
+var vis_gpp     = {min: 100, max: 3500, palette:pkg_vis.colors.RdYlGn[11]};
+var lg_gpp      = pkg_vis.grad_legend(vis_gpp, 'GPP', false); 
+pkg_vis.add_lgds([lg_et, lg_gpp]);
+
+Map.addLayer(imgcol.select("GPP"), vis_gpp, 'GPP');
 Map.addLayer(imgcol.select("Ec"), vis_et, 'Ec');
